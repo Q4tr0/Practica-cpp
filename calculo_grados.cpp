eed359390b9fd95f01ea52e8e50719dde25c7050
Guillermocala <guillermo.cala7@gmail.com>
2018-09-18 21:15:02 -0500

    segunda version calculo_grados

--- calculo_grados.cpp
+++ calculo_grados.cpp
@@ -23,42 +23,53 @@ int calculo_angulos(int numero);
 
 int main(){
 	int numero;
-	bool modificador = true; // se define la variable que dará activacion al bucle while
-	cout << "ingrese el numero de grados del angulo entre 0 y 360: ";
-	cin >> numero;
+	//se define la variable que dará activacion al bucle while
+	bool modificador1 = true; 
+	//se define la variable que dará activacion al bucle while que esta anidado dentro del primer bucle
+	bool modificador2 = true; 
+	
 	//el bucle toma la variable modificador y verifica que se cumpla la condicion, activando el bucle
-	while (modificador == true){
-		calculo_angulos(numero); //se instancia o llama a la funcion calculo_angulos tomando como argumento la variable numero que se le ha asignado un valor previamente
-		modificador = false; // se modifica la variable de activacion de ciclo para que no se ejecute como un loop infinito
+	while (modificador1 == true){
+		cout << "CALCULO DE ANGULO SEGUN GRADOS" << endl;
+		cout << "ingrese una cantidad de grados entre 0 y 360: ";
+		cin >> numero;
+		//esta funcion (system) permite importar funciones de la consola y se usara la funcion "cls" que permite limpiar la pantalla de la consola
+		system("cls");
+		while(modificador2==true && numero > 0 && numero < 360 ){
+			//se instancia o llama a la funcion calculo_angulos tomando como argumento la variable numero que se le ha asignado un valor previamente
+			calculo_angulos(numero); 
+			/*se modifica el valor de la variable de activacion del primer bucle (maodificador1) para que no se ejecute infinitamente, se modifica aqui porque requiero que el 
+			programa me siga pdidiendo datos hasta que estos sean validos*/
+			modificador1 = false;
+			//aqui se modifica la variable de activacion del segundo bucle while (modificador2) para que no se ejecute infinitamente
+			modificador2 =false;  
+		}
 	}
-
+//esta funcion permite que no se cierre el programa inmediatamente despues de su ejecuccion
+system("pause");
 }	
 //calculo_angulos recibe como parametro un numero x y verifica si cumple ciertas condiciones presentadas dentro de esta
 int calculo_angulos(int numero){
 	if (numero==0){
-		cout << "el angulo es nulo." << endl;
+		cout << numero << " grados equivale a un angulo nulo." << endl;
 	}
 	else if (numero>0 && numero <90){
-		cout << "el angulo es agudo." << endl;
+		cout << numero << " grados equivale a un angulo agudo." << endl;
 	}
 	else if (numero==90){
-		cout << "el angulo es recto." << endl;
+		cout << numero << " grados equivale a un angulo recto." << endl;
 	}
 	else if (numero>90 && numero <180){
-		cout << "el angulo es obtuso." << endl;
+		cout << numero << " grados equivale a un angulo obtuso." << endl;
 	}
 	else if (numero==180){
-		cout << "el angulo es llano." << endl;
+		cout << numero << " grados equivale a un angulo llano." << endl;
 	}
 	else if (numero>180 && numero <360){
-		cout << "el angulo es oblicuo." << endl;
+		cout << numero << " grados equivale a un angulo oblicuo." << endl;
 	}
 	else if (numero==360){
-		cout << "el angulo es completo o angulo perigonal." << endl;
+		cout << numero << " grados equivale a un angulo completo o angulo perigonal." << endl;
 	}
-	else{
-		cout << "usted ha ingresado un valor invalido! " << endl;
-	}
-system("pause"); // esta funcion permite que no se cierre el programa inmediatamente despues de su ejecuccion
 }
 
