6de6905342a2112934498d3ac5dffc4370dca3a4
Guillermocala <guillermo.cala7@gmail.com>
2018-09-19 22:59:44 -0500

    actualizando calculo angulos

--- ejercicios_practica/calculo_grados.cpp
+++ ejercicios_practica/calculo_grados.cpp
@@ -26,9 +26,6 @@ int main(){
 	int numero;
 	//se define la variable que dará activacion al bucle while
 	bool modificador1 = true; 
-	//se define la variable que dará activacion al bucle while que esta anidado dentro del primer bucle
-	bool modificador2 = true; 
-	
 	//el bucle toma la variable modificador y verifica que se cumpla la condicion, activando el bucle
 	while (modificador1 == true){
 		cout << "CALCULO DE ANGULO SEGUN GRADOS" << endl;
@@ -36,14 +33,16 @@ int main(){
 		cin >> numero;
 		//esta funcion (system) permite importar funciones de la consola y se usara la funcion "cls" que permite limpiar la pantalla de la consola
 		system("cls");
-		while(modificador2==true && numero => 0 && numero <= 360 ){
+		while(numero >= 0 && numero <= 360 ){
+				break;
 			//se instancia o llama a la funcion calculo_angulos tomando como argumento la variable numero que se le ha asignado un valor previamente
 			calculo_angulos(numero); 
 			/*se modifica el valor de la variable de activacion del primer bucle (maodificador1) para que no se ejecute infinitamente, se modifica aqui porque requiero que el 
 			programa me siga pdidiendo datos hasta que estos sean validos*/
 			modificador1 = false;
-			//aqui se modifica la variable de activacion del segundo bucle while (modificador2) para que no se ejecute infinitamente
-			modificador2 =false;  
+			/*la funcion break termina la ejecuccion del ciclo, como ya el ciclo valido los datos y los procesó dentro de la funcion calculo_angulos
+			no es necesario seguir ejecutandola*/
+		
 		}
 	}
 //esta funcion permite que no se cierre el programa inmediatamente despues de su ejecuccion
