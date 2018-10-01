0dee215dbb5d8aa11ddece2935b949d23bded048
Guillermo Cala <35546879+Guillermocala@users.noreply.github.com>
2018-10-01 09:52:18 -0500

    Update num_primos.cpp
    añadiendo en comentario la ayuda o comentario guia para resolver el algoritmo

--- ejercicios_practica/num_primos.cpp
+++ ejercicios_practica/num_primos.cpp
@@ -6,7 +6,28 @@ proceso: for(num = 0; num <= 0; num++)
 		 	imprimir num es un numero primo
 entrada: ninguna. 
 		 constantes a trabajar: numero, contador
+=====================================================================================
+ejemplo de internet :/
+#include <iostream>
+using namespace std;
+int main(){
+   int a=0,i,n;
+         cout<<"Ingrese numero"<<endl;
+         cin>>n;
+         for(i=1;i<(n+1);i++){
+         if(n%i==0){
+             a++;
+            }
+         }
+         if(a!=2){
+              cout<<"No es Primo";
+            }else{
+                cout<<"Si es Primo";
+         }
+    return 0;
+}
 */
+
 #include <iostream>
 
 using namespace std;
