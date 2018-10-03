9061fa9c05d87e7df42bb6b71e0c34d1d1e67d25
Guillermo Cala <35546879+Guillermocala@users.noreply.github.com>
2018-10-03 08:51:57 -0500

    algoritmo terminado
    programa terminado sin análisis ni enunciado

--- ejercicios_practica/num_primos.cpp
+++ ejercicios_practica/num_primos.cpp
@@ -1,52 +1,28 @@
-/*escriba un programa que imprima todos los numeros primos, o los que pueda mostrar
-========================================ANALISIS=====================================
-salidas: enteros: grupo de numeros primos sin limites 
-proceso: for(num = 0; num <= 0; num++)
-		 si (num % 1 = 0 && num % num = 0)entonces
-		 	imprimir num es un numero primo
-entrada: ninguna. 
-		 constantes a trabajar: numero, contador
-=====================================================================================
-ejemplo de internet :/
-#include <iostream>
-using namespace std;
-int main(){
-   int a=0,i,n;
-         cout<<"Ingrese numero"<<endl;
-         cin>>n;
-         for(i=1;i<(n+1);i++){
-         if(n%i==0){
-             a++;
-            }
-         }
-         if(a!=2){
-              cout<<"No es Primo";
-            }else{
-                cout<<"Si es Primo";
-         }
-    return 0;
-}
-*/
-
 #include <iostream>
 
 using namespace std;
+
+int calculo(int a);
 
 int main(){
 	int num=1;
-	while(num <= 100){
-	
-	num +=1;
+	while(num<=100){
+		calculo(num);
+	num++;
 	}
-	
-	
-	
-	
+
 system("pause");
 return 0;
 }
+
 int calculo(int a){
-	while(){
-		
+	int cont=0, i;
+	for(i=1; i < (a+1); i++){
+		if(a%i==0){
+			cont++;
+		}
+	}
+	if(cont==2){
+		cout << a <<" es primo" << endl;
 	}
 }
